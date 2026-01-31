adf95f11edb22d79d26dc7a43342c45778309480
idurgit <idursupra@gmail.com>
2026-01-31 17:33:14 +0700

    improve EOF

--- database/database.go
+++ database/database.go
@@ -3,48 +3,26 @@ package database
 import (
 	"database/sql"
 	"log"
-	"net"
-	"net/url"
 	"strings"
 
-	_ "github.com/lib/pq"
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 func InitDB(connectionString string) (*sql.DB, error) {
-	// If connection string is a postgres URL, resolve hostname to an IPv4 address
+	// Add sslmode if not present (Supabase requires SSL)
 	if strings.HasPrefix(connectionString, "postgres://") || strings.HasPrefix(connectionString, "postgresql://") {
-		if u, err := url.Parse(connectionString); err == nil {
-			host := u.Hostname()
-			port := u.Port()
-			if port == "" {
-				port = "5432"
+		if !strings.Contains(connectionString, "sslmode=") {
+			separator := "?"
+			if strings.Contains(connectionString, "?") {
+				separator = "&"
 			}
-			if ips, err := net.LookupIP(host); err == nil {
-				for _, ip := range ips {
-					if ip4 := ip.To4(); ip4 != nil {
-						u.Host = net.JoinHostPort(ip4.String(), port)
-						log.Printf("Resolved DB host %s -> %s", host, ip4.String())
-						break
-					}
-				}
-			} else {
-				log.Printf("Could not resolve DB host %s: %v", host, err)
-			}
-			
-			// Add SSL mode if not present (Supabase requires SSL)
-			query := u.Query()
-			if !query.Has("sslmode") {
-				query.Set("sslmode", "require")
-				u.RawQuery = query.Encode()
-				log.Println("Added sslmode=require to connection string")
-			}
-			
-			connectionString = u.String()
+			connectionString = connectionString + separator + "sslmode=require"
+			log.Println("Added sslmode=require to connection string")
 		}
 	}
 
-	// Open database
-	db, err := sql.Open("postgres", connectionString)
+	// Open database with pgx driver
+	db, err := sql.Open("pgx", connectionString)
 	if err != nil {
 		return nil, err
 	}
